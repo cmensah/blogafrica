958cf36dfe7a483c41b8f9567e5de813f0a05544
cmensah <mensahclaude@gmail.com>
2013-11-29 11:21:23 +0100

    structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogafrica::Application.config.secret_key_base = 'efe5a04ec306afbf60c304189eb11eaf31bb03deef060642d400cd5fce3a25f69c3c6938943782129c70d0dc4c9d4ed1851316ed7ebb63cf1080a0e181f97ecd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blogafrica::Application.config.session_store :cookie_store, key: '_blogafrica_session'
